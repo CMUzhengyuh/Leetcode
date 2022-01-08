88c09ee3152663f700e5dc61d325a39e60a31944
Hobo965859229 <90294050+CMUzhengyuh@users.noreply.github.com>
2022-01-08 13:16:16 -0500

    1/8 - Tiktok

---
+++ BinaryTree/src/Leetcode236.java
@@ -0,0 +1,17 @@
+/**
+ * Leetcode 236 - Lowest Common Ancestor of a Binary Tree
+ */
+public class Leetcode236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null)
+            return right;
+        else if (right == null)
+            return left;
+        else
+            return root;
+    }
+}
--- BinaryTree/src/Leetcode94.java
+++ BinaryTree/src/Leetcode94.java
@@ -52,17 +52,4 @@ public class Leetcode94 {
         list.add(root.val);
         traversal(root.right, list);
     }
-
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 }
