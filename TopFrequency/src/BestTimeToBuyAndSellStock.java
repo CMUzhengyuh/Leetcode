062039c106f3159a0e3868d0ea3b75fe1f3bc5b4
Hobo965859229 <90294050+CMUzhengyuh@users.noreply.github.com>
2022-01-22 13:49:50 -0500

    1/22

---
+++ TopFrequency/src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,27 @@
+/**
+ * 14th - Leetcode 121 - Best Time to Buy and Sell Stock
+ */
+public class BestTimeToBuyAndSellStock {
+    /**
+     * Solution: Traversal
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public int maxProfit(int prices[]) {
+        int min = Integer.MAX_VALUE;
+        int res = 0;
+        for (int i = 0; i < prices.length; i++) {
+            /**
+             * Choose the minimum price to buy
+             */
+            if (prices[i] < min)
+                min = prices[i];
+            /**
+             * Choose the maximum price to sell (must keep one stock at hand)
+             */
+            else if (prices[i] - min > res)
+                res = prices[i] - min;
+        }
+        return res;
+    }
+}
